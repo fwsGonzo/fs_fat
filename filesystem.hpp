ae0e2bdd0c0656ddfef1acca2b8af051aace8d5d
fwsgonzo <fwsgonzo@hotmail.com>
2016-02-01 06:57:12 +0100

    Added missing header

---
+++ filesystem.hpp
@@ -0,0 +1,41 @@
+#ifndef FS_FILESYS_HPP
+#define FS_FILESYS_HPP
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace fs
+{
+  class FileSystem
+  {
+  public:
+    struct Dirent
+    {
+      Dirent(std::string n, uint32_t cl, uint32_t sz, uint8_t attr)
+        : name(n), cluster(cl), size(sz), attrib(attr) {}
+      
+      std::string name;
+      uint32_t cluster;
+      uint32_t size;
+      uint8_t  attrib;
+    };
+    
+    typedef std::shared_ptr<std::vector<Dirent>> dirvec_t;
+    
+    typedef std::function<void(bool)> on_mount_func;
+    typedef std::function<void(bool, dirvec_t)> on_ls_func;
+    
+    
+    // 0   = Mount MBR
+    // 1-4 = Mount VBR 1-4
+    virtual void mount(uint8_t partid, on_mount_func on_mount) = 0;
+    
+    // path is a path in the mounted filesystem
+    virtual void ls(const std::string& path, on_ls_func) = 0;
+  };
+}
+
+#endif
