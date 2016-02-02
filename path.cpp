90af1ec38db6e638860886e5dfa4fc8eae1cfdb5
fwsgonzo <fwsgonzo@hotmail.com>
2016-02-02 03:57:48 +0100

    Added path parser

---
+++ path.cpp
@@ -0,0 +1,125 @@
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2015 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "path.hpp"
+
+#include <string>
+#include <cerrno>
+
+namespace fs
+{
+	static const char PATH_SEPARATOR = '/';
+	static Path current_directory("/");
+	
+	Path::Path()
+		: Path(current_directory)
+	{
+		// uses current directory
+	}
+	Path::Path(const std::string& path)
+	{
+		// parse full path
+		this->state = parse(path);
+		
+	} // Path::Path(std::string)
+	
+	std::string Path::to_string() const
+	{
+		// build path
+		//std::stringstream ss;
+    std::string ss;
+		for (const auto& p : this->stk)
+		{
+			ss += PATH_SEPARATOR + p;
+		}
+		// append path/ to end
+		ss += PATH_SEPARATOR;
+		return ss;
+	}
+	
+	int Path::parse(const std::string& path)
+	{
+		if (path.empty())
+		{
+			// do nothing?
+			return 0;
+		}
+		
+		std::string buffer(path.size(), 0);
+		char lastChar = 0;
+		int  bufi = 0;
+		
+		for (size_t i = 0; i < path.size(); i++)
+		{
+			if (path[i] == PATH_SEPARATOR)
+			{
+				if (lastChar == PATH_SEPARATOR)
+				{	// invalid path containing // (more than one forw-slash)
+          return -EINVAL;
+				}
+				if (bufi)
+				{
+					name_added(std::string(buffer, 0, bufi));
+					bufi = 0;
+				}
+				else if (i == 0)
+				{
+					// if the first character is / separator,
+					// the path is relative to root, so clear stack
+					stk.clear();
+				}
+			}
+			else
+			{
+				buffer[bufi] = path[i];
+				bufi++;
+			}
+			lastChar = path[i];
+		} // parse path
+		if (bufi)
+		{
+			return name_added(std::string(buffer, 0, bufi));
+		}
+    return 0;
+	}
+	
+	int Path::name_added(const std::string& name)
+	{
+		//std::cout << "Path: " << toString() << " --> " << name << std::endl;
+		
+		if (name == ".")
+		{
+			// same directory
+		}
+		else if (name == "..")
+		{
+			// if the stack is empty we are at root
+			if (stk.empty())
+			{
+				// trying to go above root is an error
+				return -ENOENT;
+			}
+      stk.pop_back();
+		}
+		else
+		{
+			// otherwise treat as directory
+			stk.push_back(name);
+		}
+    return 0;
+	}
+}
---
+++ path.hpp
@@ -0,0 +1,93 @@
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2015 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#ifndef FS_PATH_HPP
+#define FS_PATH_HPP
+#include <vector>
+
+namespace fs
+{
+	class Path
+	{
+	public:
+		//! constructs Path to the current directory
+		Path();
+		//! constructs Path to @path
+		Path(const std::string& path);
+		
+    std::size_t size() const
+    {
+      return stk.size();
+    }
+    const std::string& operator [] (int i) const
+    {
+      return stk[i];
+    }
+    int getState() const { return state; }
+    
+		Path& operator = (const std::string& p)
+		{
+			stk.clear();
+			this->state = parse(p);
+      return *this;
+		}
+		Path& operator += (const std::string& p)
+		{
+			this->state = parse(p);
+      return *this;
+		}
+		Path operator + (const std::string& p) const
+		{
+			Path np  = Path(*this);
+      np.state = np.parse(p);
+      return np;
+		}
+		
+		bool operator == (const Path& p) const
+		{
+			if (stk.size() != p.stk.size()) return false;
+      return this->to_string() == p.to_string();
+		}
+    bool operator != (const Path& p) const
+    {
+      return !this->operator== (p);
+    }
+		bool operator == (const std::string& p) const
+		{
+			return *this == Path(p);
+		}
+		
+    void up()
+    {
+      if (!stk.empty())
+        stk.pop_back();
+    }
+		std::string to_string() const;
+    
+	private:
+		int parse(const std::string& path);
+		int name_added(const std::string& name);
+		std::string real_path() const;
+		
+		int state;
+    std::vector<std::string> stk;
+	};
+	
+}
+
+#endif
